e2bd17fc6d821edb4277a238fcb70bba9120e3be
loxt <emannuelloxt@hotmail.com>
2023-11-15 15:31:47 -0400

    feat: add assert pkg to test files

--- internal/domain/access_token/access_token_test.go
+++ internal/domain/access_token/access_token_test.go
@@ -1,36 +1,28 @@
 package access_token
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValuesf(t, 24, expirationTime, "expiration time should be 24 hours")
+}
+
 func TestGetNewAccessToken(t *testing.T) {
 	at := GetNewAccessToken()
-
-	if at.IsExpired() {
-		t.Error("brand new access token should not be expired")
-	}
-
-	if at.AccessToken != "" {
-		t.Error("new access token should not have defined access token id")
-	}
-
-	if at.UserId != 0 {
-		t.Error("new access token should not have an associated user id")
-	}
+	assert.False(t, at.IsExpired(), "brand new access token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new access token should not have defined access token id")
+	assert.True(t, at.UserId == 0, "new access token should not have an associated user id")
 }
 
 func TestAccessTokenIsExpired(t *testing.T) {
 	at := AccessToken{}
 
-	if !at.IsExpired() {
-		t.Error("empty access token should be expired by default")
-	}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
 
 	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
 
-	if at.IsExpired() {
-		t.Error("access token created three hours from now should not be expired")
-	}
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now should not be expired")
 }
