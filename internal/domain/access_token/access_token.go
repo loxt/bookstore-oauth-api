a87ccabbc5372d22f55e7ce7146aee2ddb8e761e
emannuel <emannuelloxt@hotmail.com>
2023-11-20 17:20:12 -0400

    feat: add different requests for api

--- internal/domain/access_token/access_token.go
+++ internal/domain/access_token/access_token.go
@@ -1,14 +1,42 @@
 package access_token
 
 import (
+	"fmt"
+	crypto_utils "github.com/loxt/bookstore-oauth-api/internal/utils/crypto"
 	"github.com/loxt/bookstore-oauth-api/internal/utils/errors"
 	"strings"
 	"time"
 )
 
 const (
-	expirationTime = 24
+	expirationTime             = 24
+	grantTypePassword          = "password"
+	grantTypeClientCredentials = "client_credentials"
 )
+
+type AccessTokenRequest struct {
+	GrantType string `json:"grant_type"`
+	Scope     string `json:"scope"`
+
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+func (at *AccessTokenRequest) Validate() *errors.RestErr {
+	switch at.GrantType {
+	case grantTypePassword:
+		break
+	case grantTypeClientCredentials:
+		break
+	default:
+		return errors.NewBadRequestError("invalid grant type parameter")
+	}
+
+	return nil
+}
 
 type AccessToken struct {
 	AccessToken string `json:"access_token"`
@@ -39,8 +67,9 @@ func (at *AccessToken) Validate() *errors.RestErr {
 	return nil
 }
 
-func GetNewAccessToken() AccessToken {
+func GetNewAccessToken(userId int64) AccessToken {
 	return AccessToken{
+		UserId:  userId,
 		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
@@ -49,4 +78,8 @@ func (at *AccessToken) IsExpired() bool {
 	now := time.Now().UTC()
 	expirationTime := time.Unix(at.Expires, 0)
 	return now.After(expirationTime)
+}
+
+func (at *AccessToken) Generate() {
+	at.AccessToken = crypto_utils.GetMd5(fmt.Sprintf("at-%d-%d-ran", at.UserId, at.Expires))
 }
